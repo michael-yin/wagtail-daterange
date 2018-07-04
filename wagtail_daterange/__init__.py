c56aba056b179cb46bfd70acf0dee9f65cc214c7
MichaelYin <hgyinjian@gmail.com>
2018-07-04 16:42:08 +0800

    update version

--- wagtail_daterange/__init__.py
+++ wagtail_daterange/__init__.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 
 __author__ = 'Michael Yin'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 
 default_app_config = 'wagtail_daterange.apps.RangeFilterConfig'
